b3ea92691055b633f5c45167083dc35f4d2faa90
pluveto <i@pluvet.com>
2021-09-06 13:21:01 +0800

    feat: 初始化 Console 版本

---
+++ ImageUpWpf.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageUpWpf.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
