2b356032e46c43c3e2345d3429f59af4bab492b6
pluveto <i@pluvet.com>
2021-09-06 23:10:05 +0800

    refa: better AppContext

--- ImageUpWpf/App.xaml.cs
+++ ImageUpWpf/App.xaml.cs
@@ -14,27 +14,12 @@ namespace ImageUpWpf
     /// </summary>
     public partial class App : Application
     {
-        public AppContext AppContext { get; }
+        public IuAppContext AppContext { get; }
         public App()
         {
-            AppContext = new AppContext();
+            AppContext = new IuAppContext();
+            AppContext.Init();
         }
     }
 
-    // 此处相当于一个全局的状态保存位置
-    public class AppContext
-    {
-        public PluginManager PluginManager { get; }
-        public IUploader DefaultUploader { get; set; }
-        public AppContext()
-        {
-            PluginManager = new PluginManager();
-            PluginManager.LoadPlugins();
-            if(PluginManager.Uploaders.Count > 0)
-            {
-                DefaultUploader = PluginManager.Uploaders.First();
-            }
-        }
-
-    }
 }
