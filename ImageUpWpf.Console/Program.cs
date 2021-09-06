72c2018bc8931e400617815fec2d9a05bd347b2d
pluveto <i@pluvet.com>
2021-09-06 23:10:17 +0800

    feat: console upload

--- ImageUpWpf.Console/Program.cs
+++ ImageUpWpf.Console/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using ImageUpWpf.Core;
+using NLog;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace ImageUpWpf.Console
 {
     class Program
     {
+        private static Logger logger;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Trace("App startup");
+            var context = CreateContext();
+            logger.Trace("Context created");
+
+            try
+            {
+                var task = Task.Run(async () =>
+                {
+                    System.Console.WriteLine(
+                       TyporaFormatter.Format(
+                           await new UploadTask
+                           {
+                               UploadMode = ChainUploadMode.Parallel,
+                               NamingTemplate = context.AppConfig.NamingTemplate
+                           }
+                           .AddChainUploaders(context.ChainUploaders)
+                           .AddFiles(args, AddFileCallback)
+                           .Run()
+                       )
+                   );
+                });
+                task.Wait();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.Message);
+                throw e;
+            }
+
+        }
+
+        private static IuAppContext CreateContext()
+        {
+            var context = new IuAppContext();
+            context.Init();
+            return context;
+        }
+
+        private static void AddFileCallback(object sender, TaskGroupCreatingResult e)
+        {
+            var r = !e.HasError ? "SUCC" : "FAIL";
+            var err = !e.HasError ? "" : $", Error Message: {e.Message}";
+            logger.Info($"{r}: {e.FileName}{err}");
         }
     }
 }
---
+++ ImageUpWpf.Console/TyporaFormatter.cs
@@ -0,0 +1,35 @@
+﻿using ImageUpWpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageUpWpf.Console
+{
+    public class TyporaFormatter
+    {
+        public static string Format(Dictionary<string, TaskGroup> ret)
+        {
+            if (ret.Count == 0)
+            {
+                return "Nothing to do.";
+            }
+            if (ret.Any(i => string.IsNullOrEmpty(i.Value.GetUrl())))
+            {
+                return "Upload Failed!";
+            }
+            var sb = new StringBuilder();
+            sb.AppendLine("Upload Success:");
+            foreach (var (guid, item) in ret)
+            {
+                sb.AppendLine(item.GetUrl());
+            }
+            return sb.ToString();
+        }
+
+        internal static string Format(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
