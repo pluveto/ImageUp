36d15f6d6f8f23804ec4e081d1a932fa90c79f74
pluveto <i@pluvet.com>
2021-12-05 10:29:25 +0800

    feat: FileUtil & TimeUtil

---
+++ ImageUpWpf/Util/FileUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ImageUpWpf.Util
+{
+    public static class FileUtil
+    {
+
+        public static Stream BitmapToJpegStream(BitmapSource src)
+        {            
+            Stream s = new MemoryStream();
+            var enc = new JpegBitmapEncoder();
+            enc.Frames.Add(BitmapFrame.Create(new CachedBitmap(src, BitmapCreateOptions.None, BitmapCacheOption.OnLoad)));
+            enc.Save(s);
+            return s;
+        }
+
+        public static void SaveStream(string fileFullPath, Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            FileStream fileStream = File.Create(fileFullPath, (int)stream.Length);
+            byte[] buffer = new byte[stream.Length];
+            // 将 stream 暂存到 buffer
+            stream.Read(buffer, 0, buffer.Length);
+            // Use write method to write to the file specified above
+            fileStream.Write(buffer, 0, buffer.Length);
+            //Close the filestream
+            fileStream.Close();
+        }
+    }
+}
---
+++ ImageUpWpf/Util/TimeUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageUpWpf.Util
+{
+    public static class TimeUtil
+    {
+        /// <summary>
+        /// 获取当前 unix 以 s 记的时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long Timestamp()
+        {
+            return (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+        /// <summary>
+        /// 把字符串时间转换为 unix 时间戳
+        /// </summary>
+        /// <param name="expirationDate"></param>
+        /// <returns></returns>
+        public static long Parse(string expirationDate)
+        {
+            return (long)DateTime.Parse(expirationDate).ToUniversalTime().Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        public static string TimeToDate(long? expiredAt)
+        {
+            if (null == expiredAt)
+            {
+                return "无";
+            }
+            var dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds((long)expiredAt);
+            var dateTime = dateTimeOffset.UtcDateTime;
+            return dateTime.ToString("yyyy-MM-dd");
+
+        }
+    }
+}
